f6073e5dcaa38f471001ba84f30fe6938acca99c
Murilo <mbalves1@outlook.com>
2024-03-26 13:41:27 -0300

    [feat]: async test course

--- async/async-example.test.js
+++ async/async-example.test.js
@@ -1,3 +1,32 @@
 import { it,expect } from 'vitest'
 
-it('should generate a token value')
+import { generateToken, generateTokenPromise } from './async-example.js'
+
+it('should generate a token value', (done) => {
+  const testUserEmail = 'teste@email.com'
+
+  generateToken(testUserEmail, (err, token) => {
+    try {
+      expect(token).toBeDefined()
+      // expect(token).toBe(2)
+      done()
+    } catch(err) {
+      done(err)
+    }
+  })
+})
+
+it('should generate a token value', () => {
+  const testUserEmail = 'teste@email.com'
+
+  expect(generateTokenPromise(testUserEmail)).resolves.toBeDefined()
+})
+
+
+it('should generate a token value', async () => {
+  const testUserEmail = 'teste@email.com'
+
+  const token = await generateTokenPromise(testUserEmail)
+
+  expect(token).toBeDefined()
+})
